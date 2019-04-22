1bb9ba2224ba1af79c1e552f90cd6311a3b49d1d
KorDima01 <kor.dima01@gmail.com>
2019-04-22 12:08:54 +0300

    6.*

--- Practicks/cpp/h/Programm6_1.h
+++ Practicks/cpp/h/Programm6_1.h
@@ -1,8 +1,60 @@
 #ifndef Programm6_1
 #define Programm6_1
+class Property//класс собственность
+{
+	protected:
+		double worth;//стоимость
+	public:
+		Property(double cost): worth(cost) {};
+		virtual ~Property() {};
+		virtual double tax() = 0;//виртуальная функция налогов
+};
+class Appartment : public Property// класс квартира
+{
+	public:
+		Appartment(double cost) : Property(cost) {};
+		double tax()
+		{
+			return worth/1000;
+		}
+};
+class Car : public Property// класс машина
+{
+		public:
+			Car(double cost) : Property(cost) {};
+			double tax()
+			{
+				return worth / 200;
+			}
+};
+class CountryHouse : public Property// класс дача
+{
+	public:
+		CountryHouse(double cost) : Property(cost) {};
+		double tax()
+		{
+			return worth / 500;
+		}
+
+};
 
 void Programma6_1(){
-//IN-CONSTRUCT
+  Property *properties[7];//массив указателей на Property
+  	double cost;
+  	for (int i = 0; i < 7; i++)
+  	{
+  		cout << "Введите стоимость" << endl;
+  		cin >> cost;
+  		if (i < 3)// первые 2 - класс "квартира"
+  			properties[i] = new Appartment(cost);
+  		else if (i == 3 || i == 4)// следующие 2 - класс машина
+  			properties[i] = new Car(cost);
+  		else//последние 2 - класс "дача"
+  			properties[i] = new CountryHouse(cost);
+  	}
+  	cout << "Taxes:" << endl;
+  	for (int i = 0; i < 7; i++) cout << (properties[i]->tax()) << endl;//вывод налогов
+  	for (int i = 0; i < 7; i++) delete properties[i];//удаление массива
 }
 
 #endif
--- Practicks/cpp/h/Programm6_2.h
+++ Practicks/cpp/h/Programm6_2.h
@@ -1,8 +1,266 @@
 #ifndef Programm6_2
 #define Programm6_2
+class Expression// абстрактный класс "выражение"
+{
+	protected:
+		double a;
+	public:
+		Expression(double b = 0) : a(b) {};//конструктор
+		virtual void print() = 0;//виртуальная функция для печати выражения
+		virtual double result(double b) = 0;// виртуальная функция результата
+		virtual double derivative() = 0;// виртуальная функция производной
+		virtual void copy(double b) = 0;// виртуальная функция для копирования выражения
+		virtual ~Expression() {};
+};
+class Cons : Expression// класс "константа"
+{
+	public:
+		Cons(double b = 0) : Expression(b) {};
+		void print()
+		{
+			cout << "Const: ";
+		}
+		double result(double b)
+		{
+			return a;
+		}
+		double derivative()
+		{
+			return 0;
+		}
+		void copy(double b)
+		{
 
+		}
+};
+class Var : Expression// класс "переменная"
+{
+	public:
+		Var(double b = 0) : Expression(b) {};
+		void print()
+		{
+			cout << "Variable: ";
+		}
+		double result(double b)
+		{
+			return b;
+		}
+		double derivative()
+		{
+			return 1;
+		}
+		void copy(double b)
+		{
+			a = b;
+		}
+};
+class Sum : Expression// класс "сумма"
+{
+	public:
+		Sum(double b = 0) : Expression(b) {};
+		void print()
+		{
+			cout << "Sum: ";
+		}
+		double result(double b)
+		{
+			return a+b;
+		}
+		double derivative()
+		{
+			return 1;
+		}
+		void copy(double b)
+		{
+			a = a + b;
+		}
+};
+class Dif : Expression// класс "разность"
+{
+	public:
+		Dif(double b = 0) : Expression(b) {};
+		void print()
+		{
+			cout << "Difference: ";
+		}
+		double result(double b)
+		{
+			return a-b;
+		}
+		double derivative()
+		{
+			return 1;
+		}
+		void copy(double b)
+		{
+			a = a - b;
+		}
+};
+class Mult : Expression// класс "произведение"
+{
+	public:
+		Mult(double b = 0) : Expression(b) {};
+		void print()
+		{
+			cout << "Multiply: ";
+		}
+		double result(double b)
+		{
+			return a * b;
+		}
+		double derivative()
+		{
+			return 1;
+		}
+		void copy(double b)
+		{
+			a = a * b;
+		}
+};
+class Pri : Expression// класс "частное"
+{
+	public:
+		Pri(double b = 0) : Expression(b) {};
+		void print()
+		{
+			cout << "Quotient: ";
+		}
+		double result(double b)
+		{
+			if (b!=0)
+				return a / b;
+			else return 0;
+		}
+		double derivative()
+		{
+			return 1;
+		}
+		void copy(double b)
+		{
+			if (b != 0)
+				a = a / b;
+			else a = 0;
+		}
+};
+class Sin : Expression// класс "синус"
+{
+	public:
+		Sin(double b = 0) : Expression(b) {};
+		void print()
+		{
+			cout << "Sin: ";
+		}
+		double result(double b)
+		{
+			return sin(a);
+		}
+		double derivative()
+		{
+			return cos(a);
+		}
+		void copy(double b)
+		{
+			a = b;
+		}
+};
+class Cos : Expression// класс "косинус"
+{
+	public:
+		Cos(double b = 0) : Expression(b) {};
+		void print()
+		{
+			cout << "Cos: ";
+		}
+		double result(double b)
+		{
+			return cos(b);
+		}
+		double derivative()
+		{
+			return -sin(a);
+		}
+		void copy(double b)
+		{
+			a = b;
+		}
+};
+class Exp : Expression// класс "экспонента"
+{
+	public:
+		Exp(double b = 0) : Expression(b) {};
+		void print()
+		{
+			cout << "Exp: ";
+		}
+		double result(double b)
+		{
+			return exp(b);
+		}
+		double derivative()
+		{
+			return  exp(a);
+		}
+		void copy(double b)
+		{
+			a = b;
+		}
+};
+class Ln : Expression// класс "логарифм"
+{
+	public:
+		Ln(double b = 0) : Expression(b) {};
+		void print()
+		{
+			cout << "Log: ";
+		}
+		double result(double b)
+		{
+			return log(b);
+		}
+		double derivative()
+		{
+			return  1/a;
+		}
+		void copy(double b)
+		{
+			a = b;
+		}
+};
 void Programma6_2(){
-//IN-CONSTRUCT
+  double a, b;
+  	cout << "Введите a, b" << endl;
+  	cin >> a >> b;
+  	Cons cnst = a;
+    cout << "Резульат----------l----------Производная" << endl;
+  	cnst.print();
+  	cout <<  cnst.result(b) << " ---------l--------- " << cnst.derivative() << endl;
+  	Var v = a;
+  	v.print();
+  	cout <<  v.result(b) << " ---------l--------- " << v.derivative() << endl;
+  	Sum s = a;
+  	s.print();
+  	cout <<  s.result(b) << " ---------l--------- " << s.derivative() << endl;
+  	Dif d = a;
+  	d.print();
+  	cout <<  d.result(b) << " ---------l--------- " << d.derivative() << endl;
+  	Mult m = a;
+  	m.print();
+  	cout <<  m.result(b) << " ---------l--------- " << m.derivative() << endl;
+  	Pri p = a;
+  	p.print();
+  	cout <<  p.result(b) << " ---------l--------- " << p.derivative() << endl;
+  	Sin sn = a;
+  	sn.print();
+  	cout <<  sn.result(b) << " ---------l--------- " << sn.derivative() << endl;
+  	Cos cs = a;
+  	cs.print();
+  	cout <<  cs.result(b) << " ---------l--------- " << cs.derivative() << endl;
+  	Exp e = a;
+  	e.print();
+  	cout <<  e.result(b) << " ---------l--------- " << e.derivative() << endl;
+  	Ln l = a;
+  	l.print();
+  	cout <<  l.result(b) << " ---------l--------- " << l.derivative() << endl;
 }
 
 #endif
