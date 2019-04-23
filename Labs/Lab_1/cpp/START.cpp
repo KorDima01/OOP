cfdff30557516c3ded990cfa8fec72cc566b8025
KorDima01 <kor.dima01@gmail.com>
2019-04-23 16:10:23 +0300

    lab_1

--- Labs/Lab_1/cpp/START.cpp
+++ Labs/Lab_1/cpp/START.cpp
@@ -1,6 +1,8 @@
-#include "\h/Start.h"
-int int main()
+#include "../h/Start.h"
+int main(int argc, char *argv[])
 {
-    cout << "Hello World!" << "\n";
+    cout << "Hello World!";
+    cout << "\n \n \nВведите любое значение, для закрытия окна." << "\n";
+    cin.get();
     return 0;
 }
