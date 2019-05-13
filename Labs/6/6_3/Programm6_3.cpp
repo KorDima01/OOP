a1f9d8582e3f1611871faa7d56cf1b212d09c539
KorDima01 <kor.dima01@gmail.com>
2019-05-14 02:41:30 +0300

    labs

--- Labs/6/6_3/Programm6_3.cpp
+++ Labs/6/6_3/Programm6_3.cpp
@@ -1,49 +1,29 @@
-﻿#include <iostream>
-#include <iomanip>
+#include <iostream>
 #include <string>
-using namespace std;
+#include <vector>
 
-class Animal {
-	string name;
-	int years;
+using namespace  std;
+
+
+class Animal3{
+    string name;
+    int years;
 public:
-	Animal(string nm = "Tom", int ys = 0) :name(nm), years(ys) {}
-	void setName(string name) {
-		this->name = name;
-	}
-	string getName() {
-		return name;
-	}
-	virtual void say() = 0 {}
+    Animal3(string nm = "Tom",int ys = 0):name(nm),years(ys){}
 };
-class Dog :public Animal {
-	string breed;
+class Dog:Animal3{
+    string breed;
 public:
-	Dog(string brd) :breed(brd) {}
-	void say() override  {
-		cout << "Woof" << endl;
-	}
+    Dog(string brd):breed(brd){}
 };
-class Cat :public Animal {
-	int childs;
+class Cat:Animal3{
+    int childs;
 public:
-	Cat(int chld) :childs(chld) {}
-	void say() override {
-		cout << "Meow" << endl;
-	}
+    Cat(int chld):childs(chld){}
 };
 
-
-int main(int argc, const char* argv[]) {
-	Dog d("Taksa");
-	Cat c(3);
-
-	d.setName("Dog");
-	c.setName("Cat");
-	cout << d.getName() << ": ";
-	d.say();
-	cout << c.getName() << ": ";
-	c.say();
+int main() {
+    Dog myDog("Buldog");
 
     cout << "\n \n \n Введите любое значение, для закрытия окна." << "\n";
     cin.get();
